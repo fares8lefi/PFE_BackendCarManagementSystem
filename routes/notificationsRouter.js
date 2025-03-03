01fea63dfd1bb967f622d1715cdf31ed64285387
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-03 13:14:32 +0100

    Update notifcations router

--- routes/notificationsRouter.js
+++ routes/notificationsRouter.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router(); 
 const notification = require('../controllers/notificationController');
+const {requireAuthUser} = require('../middlewares/authMiddelwares');
+
 
 router.post("/createNotification", notification.createNotification);
 router.post("/broadcastNotifToAll", notification.broadcastNotifToAll);
+router.delete("/deleteDistributedNotifications", notification.deleteDistributedNotifications);
+router.delete("/deleteAllUserNotifications",requireAuthUser,notification.deleteAllUserNotifications);
+router.get("/getUserNotifications",requireAuthUser,notification.getUserNotifications);
 
 
 module.exports = router;
