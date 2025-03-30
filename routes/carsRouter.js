cd946c19fc7465dac34e7cb2d2c9beae9b1674ac
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-30 15:52:42 +0200

    Add mideleware to the addCarImages router

--- routes/carsRouter.js
+++ routes/carsRouter.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router(); 
 const carController = require('../controllers/carControllers');
 const uploadfile = require('../middlewares/uploadList')
+const {requireAuthUser} = require('../middlewares/authMiddelwares');
 
-
-router.post('/addCarImages', uploadfile.array('images', 10), carController.addCarImages);
+router.post('/addCarImages',requireAuthUser,uploadfile.array('images', 10), carController.addCarImages);
 router.get('/getAllCars', carController.getAllCars);
 router.get('/getCarById/:id', carController.getCarById);
 router.get('/getAllCarsByMarque', carController.getAllCarsByMarque);
