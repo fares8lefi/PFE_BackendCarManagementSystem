862cbfdc509d64ac9a3abd3f374f9d3ea3830b68
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-04-06 19:44:01 +0200

    add index to Notification schema

--- models/notificationSchema.js
+++ models/notificationSchema.js
@@ -4,10 +4,14 @@ const notificationSchema = new mongoose.Schema({
   content: { type: String, required: true },
   read: { type: Boolean, default: false },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true  },
+  link: {
+    type: String,
+    required: false
+  },
 },
 { timestamps: true }
 );
-
+notificationSchema.index({ recipient: 1, read: 1, createdAt: -1 });
 const Notification = mongoose.model('Notification', notificationSchema);
 
 module.exports = Notification;
