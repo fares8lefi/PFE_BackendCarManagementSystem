b587a74aeecf94f1045151f25b7f44d70a4f5eba
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-05-12 16:31:47 +0200

    update comment router

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -6,5 +6,6 @@ const {requireAuthUser} = require('../middlewares/authMiddelwares');
 router.post("/addComment",requireAuthUser,commentController.addComment);
 router.delete("/deleteComment",requireAuthUser, commentController.deleteComment);
 router.get("/getCommentsByCar/:carId",commentController.getCommentsByCar);
-
+router.put("/updateComment",requireAuthUser, commentController.updateComment);
+router.delete("/adminDeleteComment",requireAuthUser, commentController.DeleteCommentByAdmin);
 module.exports = router;
