505bf54c50a2d4264ae3b537414d48a21c2d5eee
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-06-09 15:03:31 +0200

    update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS config améliorée
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: " http://localhost:5173",
   methods: "GET,POST,PUT,DELETE ,PATCH",
   allowedHeaders: "Content-Type,Authorization ,Cache-Control",
   credentials: true
